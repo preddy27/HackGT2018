02e31ffe7ca5192d4588f333e912f2bb39290296
Valeria <val.lopez1011@gmail.com>
2018-10-20 12:23:51 -0400

    json sorting fx
    puts the user's address and polling address and current races into a dictionary. for the contest thing though, you have to loop through it using jinja to get all the individual information

--- main.py
+++ main.py
@@ -23,18 +23,48 @@ class HomePage(webapp2.RequestHandler):
         self.response.write(main_template.render())
 
 class InfoPage(webapp2.RequestHandler):
+    def sort_info(self, json_results):
+        voter_info = {}
+
+        voter_info['election_name'] = json_results['election']['name']
+        voter_info['day'] = json_results['election']['electionDay']
+        line1 = json_results['normalizedInput']['line1']
+        city = json_results['normalizedInput']['city']
+        state = json_results['normalizedInput']['state']
+        zip = json_results['normalizedInput']['zip']
+        user_address = "%s, %s, %s %s" % (line1, city, state, zip)
+        voter_info['user_address'] = user_address
+
+        location = json_results['pollingLocations'][0]
+        voter_info['polling_name'] = location['address']['locationName']
+        line1 = location['address']['line1']
+        city = location['address']['city']
+        state = location['address']['state']
+        zip = location['address']['zip']
+        hours = location['pollingHours']
+        polling_address = '%s, %s, %s %s' % (line1, city, state, zip)
+        voter_info['polling_address'] = polling_address
+
+        voter_info['contests'] = json_results['contests']
+
+        return voter_info
+
+
     def post(self):
         user_address = self.request.get('address')
         url = "https://www.googleapis.com/civicinfo/v2/voterinfo"
         api_key = 'AIzaSyBTh58yBdRemPRhsEsJ4PipEGKqdHUY_XA'
+
         payload = {'address': user_address, 'key': api_key}
         res = requests.get(url, params=payload) #json results about the voters infopage
         json_res = json.loads(res.text)
 
-        test = {'results': json_res}
+        json_res = self.sort_info(json_res)
+
+        res_dict = {'results': json_res}
 
         info_template = jinja_current_directory.get_template('templates/infopage.html')
-        self.response.write(info_template.render(test))
+        self.response.write(info_template.render(res_dict))
 
 app = webapp2.WSGIApplication([
     ('/', HomePage),
