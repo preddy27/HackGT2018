865e97cd757f5bb26357acc580c63fce3f32efe4
Valeria <val.lopez1011@gmail.com>
2018-10-19 22:31:44 -0400

    Jinja and webapp stuff
    texhy stuff required for webapp and jinja and requests

--- main.py
+++ main.py
@@ -3,3 +3,18 @@ import os
 import jinja2
 import ast
 from google.appengine.api import urlfetch
+import requests
+import requests_toolbelt.adapters.appengine
+import json
+
+# Use the App Engine Requests adapter. This makes sure that Requests uses URLFetch.
+requests_toolbelt.adapters.appengine.monkeypatch()
+
+jinja_current_directory = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions = ['jinja2.ext.autoescape'],
+    autoescape = True)
+
+app = webapp2.WSGIApplication([
+    ('/', HomePage)
+], debug=True)
