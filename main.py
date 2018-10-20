861b309a5b95af10a3dc4494f5645fd99056af2d
Valeria <val.lopez1011@gmail.com>
2018-10-20 00:29:51 -0400

    loads api results
    albeit ugly you can get the voter info now

--- main.py
+++ main.py
@@ -24,9 +24,17 @@ class HomePage(webapp2.RequestHandler):
 
 class InfoPage(webapp2.RequestHandler):
     def post(self):
-        #gonna write some code later that does stuff with the address
+        user_address = self.request.get('address')
+        url = "https://www.googleapis.com/civicinfo/v2/voterinfo"
+        api_key = 'AIzaSyBTh58yBdRemPRhsEsJ4PipEGKqdHUY_XA'
+        payload = {'address': user_address, 'key': api_key}
+        res = requests.get(url, params=payload) #json results about the voters infopage
+        json_res = json.loads(res.text)
+
+        test = {'results': json_res}
+
         info_template = jinja_current_directory.get_template('templates/infopage.html')
-        self.response.write(info_template.render())
+        self.response.write(info_template.render(test))
 
 app = webapp2.WSGIApplication([
     ('/', HomePage),
